9e8a90a711a80b435a13989e9ee7b08e9b404c27
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-11-21 16:38:45 +0400

    inspect code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,21 @@ public class Main {
    * @param matrix матрица.
    * @return Транспонированная матрица.
    */
-  @SuppressWarnings({"checkstyle:MultipleVariableDeclarations", "checkstyle:Indentation", "checkstyle:LeftCurly", "checkstyle:NeedBraces", "checkstyle:LineLength"})
+  @SuppressWarnings({"checkstyle:MultipleVariableDeclarations",
+          "checkstyle:Indentation",
+          "checkstyle:LeftCurly",
+          "checkstyle:NeedBraces",
+          "checkstyle:LineLength"})
   public static int[][] transposeMatrix(int[][] matrix) {
-      int [][] tr = new int [matrix[0].length][matrix.length];
-      int n = matrix[0].length;
-      int m = matrix.length;
-      for (int i = 0; i < m; i++)
-      {
-          for (int j = 0; j < n; j++) tr[j][i] = matrix[i][j];
+    int [][] tr = new int [matrix[0].length][matrix.length];
+    int n = matrix[0].length;
+    int m = matrix.length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        tr[j][i] = matrix[i][j];
       }
-      return tr;
+    }
+    return tr;
   }
 }
 
