02576597527c6910f175ed2d9f3f669b3b2839e1
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-11-21 16:21:49 +0400

    добавлена функция транспонирование матрицы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,13 @@ public class Main {
    */
   @SuppressWarnings({"checkstyle:MultipleVariableDeclarations", "checkstyle:Indentation", "checkstyle:LeftCurly", "checkstyle:NeedBraces", "checkstyle:LineLength"})
   public static int[][] transposeMatrix(int[][] matrix) {
-    int [][] tr = new int [matrix[0].length][matrix.length];
-    int n = matrix[0].length;
-    int m = matrix.length;
+      int [][] tr = new int [matrix[0].length][matrix.length];
+      int n = matrix[0].length;
+      int m = matrix.length;
       for (int i = 0; i < m; i++)
       {
-      for (int j = 0; j < n; j++) tr[j][i] = matrix[i][j];
-    }
+          for (int j = 0; j < n; j++) tr[j][i] = matrix[i][j];
+      }
       return tr;
   }
 }
