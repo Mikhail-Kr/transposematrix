3118ac3f7051fe34fadf8e00f9487729e26e1613
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-11-22 16:19:55 +0400

    изменено имя матрицы, исключены переменные m,n

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,13 @@ public class Main {
    * @return Транспонированная матрица.
    */
   public static int[][] transposeMatrix(int[][] matrix) {
-    int [][] TransposedMatrix = new int [matrix[0].length][matrix.length];
-    int n = matrix.length;
-    int m = matrix[0].length;
-      for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        TransposedMatrix[j][i] = matrix[i][j];
+    int [][] transposedMatrix = new int [matrix[0].length][matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        transposedMatrix[j][i] = matrix[i][j];
       }
     }
-    return TransposedMatrix;
+    return transposedMatrix;
   }
 }
 
