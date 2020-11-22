60d86465299d085602fa0b55ba346316287e1979
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-11-22 19:51:40 +0400

    Revert "изменено имя матрицы, исключены переменные m,n"
    This reverts commit 3118ac3f7051fe34fadf8e00f9487729e26e1613.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,15 @@ public class Main {
    * @return Транспонированная матрица.
    */
   public static int[][] transposeMatrix(int[][] matrix) {
-    int [][] transposedMatrix = new int [matrix[0].length][matrix.length];
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
-        transposedMatrix[j][i] = matrix[i][j];
+    int [][] TransposedMatrix = new int [matrix[0].length][matrix.length];
+    int n = matrix.length;
+    int m = matrix[0].length;
+      for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        TransposedMatrix[j][i] = matrix[i][j];
       }
     }
-    return transposedMatrix;
+    return TransposedMatrix;
   }
 }
 
