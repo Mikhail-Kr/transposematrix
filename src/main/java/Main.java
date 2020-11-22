159428af086672d5c0e66c7b3bef199f19c8b22c
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-11-22 16:15:12 +0400

    изменено имя матрицы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,10 @@ public class Main {
    */
   public static int[][] transposeMatrix(int[][] matrix) {
     int [][] TransposedMatrix = new int [matrix[0].length][matrix.length];
-      for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
+    int n = matrix.length;
+    int m = matrix[0].length;
+      for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
         TransposedMatrix[j][i] = matrix[i][j];
       }
     }
