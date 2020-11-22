3c489312e00fbccc1fbadb8ab51edad05b84a3c4
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-11-22 20:01:57 +0400

    исправление ошибок

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
    * @return Транспонированная матрица.
    */
   public static int[][] transposeMatrix(int[][] matrix) {
-     int [][] transposedMatrix = new int [matrix[0].length][matrix.length];
+    int [][] transposedMatrix = new int [matrix[0].length][matrix.length];
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[0].length; j++) {
         transposedMatrix[j][i] = matrix[i][j];
